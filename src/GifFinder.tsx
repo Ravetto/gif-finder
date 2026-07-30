febe0890e81b73e3282541e8d3494ddf6a03a0c8
Ravetto <leandro.agustin20@outlook.com>
2026-07-30 14:49:32 -0300

    Feact: Getting Querys from input & getting in previusSearches

--- src/GifFinder.tsx
+++ src/GifFinder.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { GifList } from "./gifs/components/GifList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs-mock"
@@ -6,13 +7,37 @@ import { SearchBar } from "./shared/components/SearchBar"
 
 
 export const GifFinder = () => {
+    const [previousTerm, setPreviousTerm] = useState(['dragon ball z'])
+
+    const handleTermClicked = (term: string) => {
+        console.log({ term })
+    }
+
+    const handleSearch = (query: string = '') => {
+        query = query.trim().toLowerCase();
+
+        if (query.length === 0) return;
+
+        if (previousTerm.includes(query)) return;
+
+        setPreviousTerm([query, ...previousTerm].slice(0, 8))
+
+        // query.toLowerCase()
+        // query.trim()
+        // if (previousTerm.includes(query)) {
+        //     return
+        // } else (() => {
+        //     setPreviousTerm([...previousTerm, query])
+        // })
+        // console.log({ query })
+    }
     return (
         <>
             <CustomHeader title="Buscador de gifs" description="Descubre y comparte el gif perfecto" />
 
-            <SearchBar placeholder="busca lo que quieras" />
+            <SearchBar placeholder="busca lo que quieras" onQuery={handleSearch} />
 
-            <PreviousSearches searches={['goku', 'Saitama', 'Elden Ring']} />
+            <PreviousSearches searches={previousTerm} onLabelClicked={handleTermClicked} />
 
             <GifList gifS={mockGifs} />
         </>
--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,13 +1,44 @@
+import { useEffect, useState, type KeyboardEvent } from "react"
+
 interface Props {
-    placeholder: string
+    placeholder: string;
+    onQuery: (query: string) => void
 }
 
-export const SearchBar = ({ placeholder = "Buscar" }: Props) => {
+export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
+    const [query, setQuery] = useState('')
+
+    useEffect(() => {
+        const timeOutId = setTimeout(() => {
+            onQuery(query)
+        }, 700)
+
+        return () => {
+            clearTimeout(timeOutId)
+        }
+    }, [query, onQuery])
+
+    const handleSearch = () => {
+        onQuery(query)
+    }
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            handleSearch()
+        }
+    }
+
     return (
         <>
             <div className="search-container">
-                <input type="text" name="" id="" placeholder={placeholder} />
-                <button>Buscar</button>
+                <input
+                    type="text"
+                    placeholder={placeholder}
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
+                <button onClick={handleSearch}>Buscar</button>
             </div>
         </>
     )
